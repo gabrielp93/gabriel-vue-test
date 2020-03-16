0cebddee1bb511880755ca14ad015cfe30dbfc45
Gabriel Porto <gabrielporto93@gmail.com>
2020-03-16 11:12:02 -0300

    teste de componente

---
+++ src/components/Card.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="card">
+    <h1>Teste de componente</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  props: {
+    msg: String
+  }
+}
+</script>
